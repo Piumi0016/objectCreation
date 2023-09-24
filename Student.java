359bcf3ec5aa2525dffa8ceb37196207f9aacf96
P D P S De Almeida <140097225+Piumi0016@users.noreply.github.com>
2023-09-24 20:04:01 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,13 @@
+class Student{
+    String name1 = "Sam";
+    String name2 = "John";
+    //main class
+    public static void main(String[] args) {
+        Student myObj = new Student();
+        Student myObj1 = new Student();
+
+        //display objects
+        System.out.println(myObj.name1);
+        System.out.println(myObj1.name2);
+    }
+}
